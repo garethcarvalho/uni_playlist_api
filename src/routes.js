c6a6255da57efb272b41166177a16742d5f9bb44
garethcarvalho <gcarv023@mtroyal.ca>
2023-10-31 08:55:15 -0600

    added route for creating a user

--- src/routes.js
+++ src/routes.js
@@ -15,4 +15,34 @@ module.exports.register = (app, database) => {
         res.status(200).send(JSON.stringify(records)).end();
     });
 
+    app.post('/api/users', async (req, res) => {
+        let username = req.username;
+        let password = req.password;
+
+        // Here we will check whether username and password are valid.
+        // Check for SQL injections, salt the password, generate userId.
+
+        // Check if user exists.
+        let query = 'SELECT count(*) AS userCount FROM Users WHERE Username = ?'
+        await database.query(query, [username], (err, result) => {
+            if (err) throw err;
+
+            if (parseInt(result[0].userCount) > 0) {
+                res.status.send("User already exists with username " + username)
+            }
+        });
+
+
+        query = 'INSERT INTO Users (Username, Salt) VALUES (?, ?)'
+        await database.query(query, [username, password], (err, result) => {
+            if (err) {
+                // Error Handling
+                // throw err;
+                res.status(404).send("User was not able to be created");
+                return;
+            }
+
+            res.status(200).send("User has been created");
+        });
+    });
 };
