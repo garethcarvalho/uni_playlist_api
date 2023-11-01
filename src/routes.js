e459201d821f7875852aecefadcd99bca9101c2d
garethcarvalho <gcarv023@mtroyal.ca>
2023-11-01 09:55:27 -0600

    Added documentation for app.post('/api/user')

--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,13 @@ module.exports.register = (app, database) => {
         res.status(200).send(JSON.stringify(records)).end();
     });
 
+    /**
+     * Route to create a new Universal Playlist user.
+     * @param {string} username - The username for the new user. Must be a username
+     * that doesn't already exist.
+     * @param {string} password - The password for the new user. Is salted and then
+     * stored in the database if the new user is successfully created.
+     */
     app.post('/api/users', async (req, res) => {
         let username = req.username;
         let password = req.password;
@@ -28,7 +35,8 @@ module.exports.register = (app, database) => {
             if (err) throw err;
 
             if (parseInt(result[0].userCount) > 0) {
-                res.status.send("User already exists with username " + username)
+                res.status.send("User already exists with username " + username).end();
+                return;
             }
         });
 
@@ -38,11 +46,11 @@ module.exports.register = (app, database) => {
             if (err) {
                 // Error Handling
                 // throw err;
-                res.status(404).send("User was not able to be created");
+                res.status(404).send("User was not able to be created").end();
                 return;
             }
 
-            res.status(200).send("User has been created");
+            res.status(200).send("User has been created").end();
         });
     });
 };
