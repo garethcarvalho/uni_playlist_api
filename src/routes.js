7e72dfa5d3a9ac42379cb05bfd825e884c7b386a
Gareth Carvalho <garethcarva@gmail.com>
2023-11-05 16:37:29 -0700

    Removed redundant route

--- src/routes.js
+++ src/routes.js
@@ -25,48 +25,6 @@ module.exports.register = (app, database) => {
      * stored in the database if the new user is successfully created.
      */
     app.post('/api/users', async (req, res) => {
-        let username = req.username;
-        let password = req.password;
-
-        // Here we will check whether username and password are valid.
-        // Check for SQL injections, salt the password, generate userId.
-
-        // Check if user exists.
-        let query = 'SELECT count(*) AS userCount FROM Users WHERE Username = ?'
-        await database.query(query, [username], (err, result) => {
-            if (err) throw err;
-            
-            // If the userCount is not equal to 0, then a user with that username
-            // already exists. Thus, we exit early.
-            if (parseInt(result[0].userCount) > 0) {
-                res.status.send("User already exists with username " + username).end();
-                return;
-            }
-        });
-
-        // We are good to create a new user. Insert new user into the database.
-        query = 'INSERT INTO Users (Username, Salt) VALUES (?, ?)'
-        await database.query(query, [username, password], (err, result) => {
-            if (err) {
-                // Error Handling
-                // throw err;
-                res.status(404).send("User was not able to be created").end();
-                return;
-            }
-
-            res.status(200).send("User has been created").end();
-        });
-    });
-
-
-    /**
-     * Route to create a new Universal Playlist user.
-     * @param {string} username - The username for the new user. Must be a username
-     * that doesn't already exist.
-     * @param {string} password - The password for the new user. Is salted and then
-     * stored in the database if the new user is successfully created.
-     */
-    app.post('/api/users', async (req, res) => {
         let username = req.query.username;
         let password = req.query.password;
 
@@ -102,15 +60,15 @@ module.exports.register = (app, database) => {
             return; // Early return if user already exists.
         }
 
+        // Insert User into Table.
         const createUserQuery = database.query(
             'INSERT INTO Users (Username, Salt) VALUES (?, ?)',
             [username, password]
         );
-
+        
         const results = await createUserQuery;
         status = "Successful";
         message = `User with the username "${username}" has been created.`;
         res.status(200).send(`$status: ${message}`).end();
-
     });
 };
